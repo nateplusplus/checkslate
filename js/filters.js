a74b302e47e2b458bee3d75b4867d61a076af162
Nathan Blair <nateblairfilm@gmail.com>
2015-10-12 23:44:38 -0500

    finished angular app!

---
+++ js/filters.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('checkslateFilters', []).filter('incomplete', function() {
+    return function (input){
+        var result = [];
+        
+        for (var i = 0; i < input.length; i++) {
+            if (!input[i].complete) {
+                result.push(input[i]);
+            }
+        }
+        return result;
+    }
+})
+.filter('complete', function() {
+    return function (input){
+        var result = [];
+        
+        for (var i = 0; i < input.length; i++) {
+            if (input[i].complete) {
+                result.push(input[i]);
+            }
+        }
+        return result;
+    }
+});
