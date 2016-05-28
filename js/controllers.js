7ee660e108bb36c91fe989208eaf88327717f364
Nathan Blair <nateblairfilm@gmail.com>
2016-05-28 15:07:37 -0500

    cleaning whitespace and extra files

--- js/controllers.js
+++ js/controllers.js
@@ -4,54 +4,54 @@ var url = 'js/model.js';
 var controllers = angular.module('checkslate.controllers', []);
 
 controllers.controller('appCtrl', function ($scope, $http) {
-    $scope.edit = false;
-    $scope.items = '';
-    $http.get(url).success(function(data){
-        $scope.items = data;
-    });
-    $scope.toggleEdit = function(){
-        if($scope.edit){
-            $scope.edit = false;
-        } else {
-            $scope.edit = true;
-        }
-    }
-    $scope.itemClicked = function(clicked){
-        
-        for ( var i = 0; i < $scope.items.length; i++ ) {
-            if ( clicked.name === $scope.items[i].name ) {
-                if ( $scope.items[i].complete === false ){
-                    $scope.items[i].complete = true;
-                } else {
-                    $scope.items[i].complete = false;
-                }
-            }
-        }
-    }
-    $scope.addItem = function(item){
-        var newItem = { "name" : item, "complete" : false };
-        console.log();
-            if ( item != undefined && /[A-Za-z]/g.test(item) ){
-                $scope.items.unshift(newItem);
-            }
-        //document.getElementById('addItemInput').value = '';
-        $scope.newItem = '';
-    }
-    $scope.keyboardHandler = function(keyEvent, item){
-        if(keyEvent.which === 13){
-            $scope.addItem(item);
-        }
-    }
-    $scope.deleteItem = function(clicked){
-        for ( var i = 0; i < $scope.items.length; i++ ) {
-            if ( clicked.name === $scope.items[i].name ) {
-                $scope.items.splice(i, 1);
-            }
-        }
-    }
-    $scope.clean = function(){
-        for ( var i = 0; i < $scope.items.length; i++ ) {
-            $scope.items[i].complete = false;
-        }
-    }
+		$scope.edit = false;
+		$scope.items = '';
+		$http.get(url).success(function(data){
+				$scope.items = data;
+		});
+		$scope.toggleEdit = function(){
+				if($scope.edit){
+						$scope.edit = false;
+				} else {
+						$scope.edit = true;
+				}
+		}
+		$scope.itemClicked = function(clicked){
+				
+				for ( var i = 0; i < $scope.items.length; i++ ) {
+						if ( clicked.name === $scope.items[i].name ) {
+								if ( $scope.items[i].complete === false ){
+										$scope.items[i].complete = true;
+								} else {
+										$scope.items[i].complete = false;
+								}
+						}
+				}
+		}
+		$scope.addItem = function(item){
+				var newItem = { "name" : item, "complete" : false };
+				console.log();
+						if ( item != undefined && /[A-Za-z]/g.test(item) ){
+								$scope.items.unshift(newItem);
+						}
+				//document.getElementById('addItemInput').value = '';
+				$scope.newItem = '';
+		}
+		$scope.keyboardHandler = function(keyEvent, item){
+				if(keyEvent.which === 13){
+						$scope.addItem(item);
+				}
+		}
+		$scope.deleteItem = function(clicked){
+				for ( var i = 0; i < $scope.items.length; i++ ) {
+						if ( clicked.name === $scope.items[i].name ) {
+								$scope.items.splice(i, 1);
+						}
+				}
+		}
+		$scope.clean = function(){
+				for ( var i = 0; i < $scope.items.length; i++ ) {
+						$scope.items[i].complete = false;
+				}
+		}
 });
--- js/model.js
+++ js/model.js
@@ -1,38 +1,38 @@
 [
-    {
-        "name"      : "Tent",
-        "complete"  : false
-    },
-    {
-        "name": "Rainfly",
-        "complete"  : false
-    },
-    {
-        "name": "Stakes",
-        "complete"  : false
-    },
-    {
-        "name": "Hammer",
-        "complete"  : false
-    },
-    {
-        "name": "Tarp",
-        "complete"  : false
-    },
-    {
-        "name": "Cooler",
-        "complete"  : false
-    },
-    {
-        "name": "Foldable Dish Bucket With Soap",
-        "complete"  : false
-    },
-    {
-        "name": "Swim Suit",
-        "complete"  : true
-    },
-    {
-        "name": "Ketchup",
-        "complete"  : true
-    }
+		{
+				"name"      : "Tent",
+				"complete"  : false
+		},
+		{
+				"name": "Rainfly",
+				"complete"  : false
+		},
+		{
+				"name": "Stakes",
+				"complete"  : false
+		},
+		{
+				"name": "Hammer",
+				"complete"  : false
+		},
+		{
+				"name": "Tarp",
+				"complete"  : false
+		},
+		{
+				"name": "Cooler",
+				"complete"  : false
+		},
+		{
+				"name": "Foldable Dish Bucket With Soap",
+				"complete"  : false
+		},
+		{
+				"name": "Swim Suit",
+				"complete"  : true
+		},
+		{
+				"name": "Ketchup",
+				"complete"  : true
+		}
 ]
